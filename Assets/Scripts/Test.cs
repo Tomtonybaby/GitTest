cd7d6173f09ad0ebe5c69efe2e2869e911403448
DingZhen <DingZhen@qq.com>
2024-08-14 13:02:38 +0800

    第五次

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("git config --global http.sslVersion tlsv1.2");
+        Debug.Log("fuck this shit");
     }
 
     // Update is called once per frame
